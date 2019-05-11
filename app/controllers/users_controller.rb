9da623705d9e98dc9ee20a60080a9a5ef0b8e851
face9363 <4181hiro@gmail.com>
2019-05-12 00:41:22 +0900

    usersのルーティング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,19 @@
 class UsersController < ApplicationController
   include UsersHelper
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+    @current_user = current_user
+  end
+
+  def update
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/users/signup',  to: 'users#new'
+  get '/users/show/:id', to: 'users#show'
+  #get '/users/edit/:id', to: 'users#edit'
+  post '/users/create', to: 'users#create'
+  post '/users/update', to: 'users#update'
 end
