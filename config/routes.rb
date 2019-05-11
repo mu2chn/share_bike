0eb195da7cf39083842647f5f9a4987e75693cb1
face9363 <4181hiro@gmail.com>
2019-05-12 03:07:29 +0900

    routing修正

--- config/routes.rb
+++ config/routes.rb
@@ -14,15 +14,16 @@ Rails.application.routes.draw do
   get '/tourists/edit', to: 'tourists#edit', as: 't-edit'
   post '/tourists/update', to: 'tourists#update'
 
-  get '/bikes', to: 'bikes#index'
-  get '/bikes/show/:id', to: 'bikes#show'
-  post '/bikes/rent/:id', to: 'bikes#rent'
+  get '/bikes', to: 'bikes#index', as: 'b-index'
+  get '/bikes/show/:id', to: 'bikes#show', as: 'b-show'
+  post '/bikes/rent/:id', to: 'bikes#rent', as: 'b-rent'
 
-  get '/bikes/mine', to: 'bikes#mine'
-  get '/bikes/edit', to: 'bike#edit'
+  get '/bikes/mine', to: 'bikes#mine', as: 'b-mine'
+  get '/bikes/edit', to: 'bikes#edit', as: 'b-edit'
 
   get '/u-login',   to: 'sessions#u_new', as: 'u-login'
   get '/t-login', to: 'sessions#t_new', as: 't-login'
   post '/login',   to: 'sessions#create', as: 'login'
   get '/logout',  to: 'sessions#destroy', as: 'logout'
+
 end
