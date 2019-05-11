3a5217b3afbd762ad6359006f8da8dd5d2c15a8f
face9363 <4181hiro@gmail.com>
2019-05-11 21:52:42 +0900

    ユーザーmodelに最低限のバリデーション

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
+  validates :name, presence: true
+  validates :email, presence: true,
+            uniqueness: { case_sensitive: false }
+  validates :terms
+end
