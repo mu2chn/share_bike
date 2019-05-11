5097c10180958bb911f93765e8cd3b76b31e9172
face9363 <4181hiro@gmail.com>
2019-05-12 00:58:16 +0900

    session追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,5 @@
+class SessionsController < ApplicationController
+  def u_new
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/users/signup',  to: 'users#new'
@@ -6,4 +8,8 @@ Rails.application.routes.draw do
   #get '/users/edit/:id', to: 'users#edit'
   post '/users/create', to: 'users#create'
   post '/users/update', to: 'users#update'
+
+  get    '/users/login',   to: 'sessions#u_new'
+  post   '/users/login',   to: 'sessions#u_create'
+  delete '/users/logout',  to: 'sessions#u_destroy'
 end
