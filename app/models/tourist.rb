fc952b7e21feb8f37938e33e57a8e6b27fba9c2f
face9363 <4181hiro@gmail.com>
2019-05-11 22:42:59 +0900

    最低限のバリデーション

--- app/models/tourist.rb
+++ app/models/tourist.rb
@@ -1,2 +1,17 @@
 class Tourist < ApplicationRecord
+  before_save do
+    self.email = email.downcase
+  end
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true,
+            length: { maximum: 50 }
+  validates :email, presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX },
+            length: { maximum: 255 }
+  validates :terms, presence: true
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
