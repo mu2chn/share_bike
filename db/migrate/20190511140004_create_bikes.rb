b2a6aec9009193762bd54852b01977cb8df1fbf5
face9363 <4181hiro@gmail.com>
2019-05-11 23:12:42 +0900

    bikeモデル作成

---
+++ db/migrate/20190511140004_create_bikes.rb
@@ -0,0 +1,14 @@
+class CreateBikes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :bikes do |t|
+      t.string :name
+      t.references :user, foreign_key: true
+      t.string :vehicle_num
+      t.string :security_area
+      t.integer :security_num
+      t.integer :status, default: 0
+      t.text :details
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190511133727) do
+ActiveRecord::Schema.define(version: 20190511140004) do
+
+  create_table "bikes", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.string "vehicle_num"
+    t.string "security_area"
+    t.integer "security_num"
+    t.integer "status", default: 0
+    t.text "details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bikes_on_user_id"
+  end
 
   create_table "tourists", force: :cascade do |t|
     t.string "name"
