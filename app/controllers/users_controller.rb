591326073261b4dbbc5b2ef365f1bfb4c0f40020
Fbase <38308360+face9363@users.noreply.github.com>
2019-05-12 02:37:46 +0900

    Revert "Session周り"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,3 @@
 class UsersController < ApplicationController
   include UsersHelper
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-  end
-
-  def show
-    @current_user = current_user
-  end
-
-  def update
-
-  end
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'root#index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get '/users/signup',  to: 'users#new'
-  get '/users/show/:id', to: 'users#show'
-  post '/users/create', to: 'users#create'
-  post '/users/update', to: 'users#update'
-
-  get '/u-login',   to: 'sessions#u_new'
-  get '/t-login', to: 'sessions#t_new'
-  post '/login',   to: 'sessions#create'
-  get '/logout',  to: 'sessions#destroy'
 end
