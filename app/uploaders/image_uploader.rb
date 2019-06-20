1127fd97aaeb7378c1818e6aea6568da10b866b3
face9363 <4181hiro@gmail.com>
2019-06-20 20:49:56 +0900

    画像削除

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,7 +30,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-     process resize_to_fill: [480, 240, "Center"]
+    process resize_to_fill: [720, 480]
+     #process resize_to_fill: [480, 240, "Center"]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
