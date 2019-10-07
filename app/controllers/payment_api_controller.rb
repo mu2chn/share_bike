828e658f32253801d34f810aa9cae7ec0c363b2b
face9363 <4181hiro@gmail.com>
2019-10-07 14:33:08 +0900

    適当にやってたらできた

--- app/controllers/payment_api_controller.rb
+++ app/controllers/payment_api_controller.rb
@@ -1,43 +1,41 @@
 class PaymentApiController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  require 'paypal-sdk-rest'
-  include PayPal::SDK::OpenIDConnect
-  include PayPal::SDK::REST
+  # require 'paypal-sdk-rest'
+  # include PayPal::SDK::OpenIDConnect
+  # include PayPal::SDK::REST
+
+  require 'paypal-checkout-sdk'
+  include PayPalCheckoutSdk::Orders
+
 
 # Update client_id, client_secret and redirect_uri
-  PayPal::SDK.configure({
-                            :openid_client_id     => "AbmTHlJhDD2_U9v4JbjXULVdrAcAAFJqb1ZH-ZbpICzZuYWjaMwiQsOoXCytKCcqK2BfP31zjMTpC7sT",
-                            :openid_client_secret => "EOo9mHVWT1T4fubdtQErBcgEXIbd3XTEiCrKjr9l3CwNzDW59PgYlcNP7dEp1DSvYyaAYkXwPexFwZ0_",
-                        })
+#   PayPal::SDK.configure({
+#                             :openid_client_id     => "AbmTHlJhDD2_U9v4JbjXULVdrAcAAFJqb1ZH-ZbpICzZuYWjaMwiQsOoXCytKCcqK2BfP31zjMTpC7sT",
+#                             :openid_client_secret => "EOo9mHVWT1T4fubdtQErBcgEXIbd3XTEiCrKjr9l3CwNzDW59PgYlcNP7dEp1DSvYyaAYkXwPexFwZ0_",
+#                         })
 
   def test
-    @payment_history = Payment.all( :count => 5 )
-
-    # List Payments
-    logger.info "List Payment:"
-    @payment_history.payments.each do |payment|
-      logger.info "  -> Payment[#{payment.id}]"
-    end
+    # @payment_history = Payment.all( :count => 5 )
+    #
+    # # List Payments
+    # logger.info "List Payment:"
+    # @payment_history.payments.each do |payment|
+    #   logger.info "  -> Payment[#{payment.id}]"
+    # end
     render about_path
   end
 
   def check
+    client_id = "AbmTHlJhDD2_U9v4JbjXULVdrAcAAFJqb1ZH-ZbpICzZuYWjaMwiQsOoXCytKCcqK2BfP31zjMTpC7sT"
+    client_secret = "EOo9mHVWT1T4fubdtQErBcgEXIbd3XTEiCrKjr9l3CwNzDW59PgYlcNP7dEp1DSvYyaAYkXwPexFwZ0_"
+    environment = PayPal::SandboxEnvironment.new(client_id, client_secret)
+    client = PayPal::PayPalHttpClient.new(environment)
     json = JSON.parse(request.body.read)
+    order = PayPalCheckoutSdk::Orders::OrdersGetRequest::new(json["orderID"])
+    client.execute(order)
 
-    # p Tokeninfo.authorize_url( :scope => "openid profile" )
-    # payment_history = Payment.all( :count => 10 )
-    # p payment_history.payments
-    # p payment_history.count
-    # p payment = Payment.find(p json["orderID"])
 
-    @payment_history = Payment.all( :count => 5 )
-
-    # List Payments
-    logger.info "List Payment:"
-    @payment_history.payments.each do |payment|
-      logger.info "  -> Payment[#{payment.id}]"
-    end
   end
 
   def unpaid
