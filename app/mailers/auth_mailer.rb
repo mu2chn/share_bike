951ac9fea3f754ede23a54a0f55f24b8756ef606
face9363 <4181hiro@gmail.com>
2019-10-22 14:20:35 +0900

    メール認証

--- app/mailers/auth_mailer.rb
+++ app/mailers/auth_mailer.rb
@@ -22,4 +22,24 @@ class AuthMailer < ApplicationMailer
     end
   end
 
+  def auth_user(user)
+    @user = user
+    mail(
+        subject: "以下のリンクを踏んでメール認証をして下さい。",
+        to: @user.email #宛先
+    ) do |format|
+      format.text
+    end
+  end
+
+  def u_complete_auth(user)
+    @user = user
+    mail(
+        subject: "メール認証が完了しました。",
+        to: @user.email #宛先
+    ) do |format|
+      format.text
+    end
+  end
+
 end
