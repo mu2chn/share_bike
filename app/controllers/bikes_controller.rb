2ee55ca2345330a912bea6814799dd379f90494d
Hiroki <face9363>
2020-03-24 01:01:43 +0900

    refactor bike

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,5 +1,7 @@
 class BikesController < ApplicationController
 
+  include BikesService
+
   def index
     dsearch = nil
     if params["dsearch"].present?
@@ -12,10 +14,7 @@ class BikesController < ApplicationController
     @prev_day = dsearch
     @date_hash = make_month
 
-    if user = current_user
-      user.update_attribute(:tutorial,
-                            tutorial(user.tutorial, 0, "ここは自転車検索ページです。日程などから自転車を探すことができます"))
-    end
+    tutorial_index(current_user)
   end
 
   def show
@@ -23,10 +22,7 @@ class BikesController < ApplicationController
     @reservations = TouristBike.where(bike_id: @bike.id)
                         .where(start_datetime: (Time.now)...Time.now.since(2.weeks))
                         .order(start_datetime: "ASC").page(params[:page]).per(4)
-    if user = current_user
-      user.update_attribute(:tutorial,
-                            tutorial(user.tutorial, 1, "ここは予約ページです。気に入った自転車の予約をすることができます。"))
-    end
+    tutorial_show(current_user)
   end
 
   def new
@@ -36,14 +32,9 @@ class BikesController < ApplicationController
   def create
     if_user do |user|
       @user = user
-      @bike = Bike.new(bike_params)
-      @bike.user_id = @user.id
-      if @bike.save
-        flash[:success] = "新しく自転車を追加しました！今度は貸し出す日程を決めましょう"
-        redirect_to u_reserve_path
-      else
-        render b_new_path
-      end
+      @bike = create_bike(bike_params, user)
+      flash[:success] = I18n.t('flash.bike.create.success.register')
+      redirect_to u_reserve_path
     end
   end
 
@@ -58,18 +49,9 @@ class BikesController < ApplicationController
     if_user do |user|
       @user = user
       @bike = Bike.find(params[:id])
-      if @bike.user_id == user.id
-        if @bike.update_attributes(bike_update_params)
-          flash[:success] = "Updated"
-          redirect_to "/bikes/edit/"+params[:id].to_s
-        else
-          flash[:warning] = "失敗しました"
-          redirect_to "/bikes/edit/"+params[:id].to_s
-        end
-      else
-        flash[:warning] = "権限がありません"
-        redirect_to root_path
-      end
+      update_bike(@bike, bike_update_params, user)
+      flash[:success] = I18n.t('flash.bike.update.success.exec')
+      redirect_to b_edit_path(bike.id)
     end
   end
 
@@ -81,35 +63,5 @@ class BikesController < ApplicationController
   def bike_update_params
     params.require(:bike).permit(:name, :details, :image, :pass)
   end
-
-  def make_month
-    pre_month = (Date.today...Date.tomorrow.end_of_month).to_a
-    next_month =  (Date.tomorrow.end_of_month..Date.today.next_month).to_a
-    if !pre_month
-      days = next_month
-    elsif !next_month
-      days = pre_month
-    else
-      days = pre_month + next_month
-    end
-    days[0, 9].map do |date|
-      [ "#{date.month}月#{date.day}日（#{[ "日", "月", "火", "水", "木", "金", "土"][date.wday]}）", date.strftime("%Y/%m/%d")]
-    end.to_a
-  end
-
-  #def search_by_date(day)
-  #  if day.nil? || day == ""
-  #    return nil
-  #  end
-  #  day = day.to_i
-  #  search_date = Date.today
-  #  if search_date.day >= day
-  #    search_date = search_date.next_month
-  #    search_date = search_date.ago((search_date.day-day).days)
-  #  else
-  #    search_date = search_date.since((day-search_date.day).days)
-  #  end
-  #  search_date
-  #end
 
 end
--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -1,2 +1,3 @@
 module ApplicationService
+
 end
